6dac6dd1b78a14bb6f593bc01985fa6275cea200
liam303111 <72682352+liam303111@users.noreply.github.com>
2020-10-25 12:55:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb;\
 rm -rf google-chrome-stable_current_amd64.deb;\
 cp /content/drive/My\ Drive/colab/* /content/;\
 unzip /content/google-chrome.zip -d /root/.config;\
+wget -O /usr/share/applications/google-chrome.desktop https://github.com/liam303111/public/raw/main/google-chrome.desktop;\
