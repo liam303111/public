1e688cf496d4c3967fbef11424f4fd39aae9d96b
liam303111 <72682352+liam303111@users.noreply.github.com>
2020-10-25 13:00:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ rm -rf google-chrome-stable_current_amd64.deb;\
 cp /content/drive/My\ Drive/colab/* /content/;\
 unzip /content/google-chrome.zip -d /root/.config;\
 wget -O /usr/share/applications/google-chrome.desktop https://github.com/liam303111/public/raw/main/google-chrome.desktop;\
+sudo apt-get purge firefox;\
